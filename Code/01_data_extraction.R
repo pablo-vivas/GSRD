c9cd205e4c5cbdf85bd7f46afea88b21298b0d9b
Pablo <vivas014@umn.edu>
2022-12-13 01:26:16 -0600

    Create code extraction

---
+++ Code/01_data_extraction.R
@@ -0,0 +1,24 @@
+#Get wd
+wd <- getwd()
+
+#Load Libraries
+library(tidyverse)
+library(rvest)
+library(stringr)
+
+#Set wd
+setwd(str_c(getwd(),"/Documents"))
+
+page <- read_html("https://iase-web.org/Conference_Proceedings.php?p=ICOTS_11_2022")
+
+pdfs <- page %>%
+  html_nodes("a") %>% 
+  html_attr("href") %>% 
+  str_subset("\\.pdf") %>% 
+  str_subset("ICOTS11_") %>%
+  str_c("https://iase-web.org/", .) %>% 
+  str_replace("\\.pdf.*","") %>% 
+  str_c(., ".pdf") %>% 
+  walk2(., basename(.), download.file, mode = "wb")
+
+setwd(wd)
